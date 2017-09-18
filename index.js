38ce1ce8a265556cf7b5df31623ef945fd85b763
bleavs <bleavitt@colgate.edu>
2017-09-18 17:43:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+  let counter = 0;
+
+  function addBump(){
+    counter++;
+  };
+
+  function getBumps(){
+    return counter;
+  };
+
+  return {
+    addBump,
+    getBumps
+  }
+
+}
+
+function createAnimal(animalType){
+
+  function addDevice(deadlyDevice){
+    return {
+      animalType: animalType,
+      deadlyDevice: deadlyDevice
+    }
+  }
+  return addDevice;
+}
+let sharkCreator = createAnimal("Shark");
+let sharkWithFrickinLaserbeam = sharkCreator("Laserbeam");
+let sharkWithFrickinCannon = sharkCreator("Cannon");
